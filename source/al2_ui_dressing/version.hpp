3cde18a39bc75b34d367d74d5955be753dd54b46
hebiiro <io.hebiiro@gmail.com>
2026-09-06 18:57:03 +0900

    🔖r9 2026年09月06日

--- source/al2_ui_dressing/version.hpp
+++ source/al2_ui_dressing/version.hpp
@@ -9,6 +9,6 @@ namespace apn::dark
 		//
 		// コンストラクタです。
 		//
-		version_t() : version_base_t(L"UIドレッシングMOD", L"🐍UIドレッシングMOD🔖", L"r8") {}
+		version_t() : version_base_t(L"UIドレッシングMOD", L"🐍UIドレッシングMOD🔖", L"r9") {}
 	} version;
 }
